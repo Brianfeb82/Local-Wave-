49154dd20a63a9e9c7365c94e4a9809faa90be5e
Brianfeb82 <brian.feb876@gmail.com>
2026-06-03 18:24:34 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-const basePath = isProd ? "/Local-Wave-" : "";
 
 const nextConfig = {
   output: "export",
-  basePath,
-  assetPrefix: basePath || undefined,
+  basePath: isProd ? "/Local-Wave-" : "",
+  assetPrefix: isProd ? "/Local-Wave-/" : "", // Ditambah / di akhir khusus untuk aset CSS/JS
   trailingSlash: true,
   images: {
     unoptimized: true
